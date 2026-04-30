8add2cbf37f4557134680ef29d5c2ce8e15ebc42
vinayak-vikram <vinayak_v@icloud.com>
2026-04-30 00:56:13 -0700

    for calculations

--- joukowsky.cpp
+++ joukowsky.cpp
@@ -6,6 +6,11 @@ std::complex<double> JoukowskyTransform::z2w(std::complex<double> z) const {
     return z + (a * a) / z;
 }
 
+// dw/dz = 1 - a^2/z^2
+std::complex<double> JoukowskyTransform::dz2w(std::complex<double> z) const {
+    return 1.0 - (a * a) / (z * z);
+}
+
 // solve z^2-wz+a^2=0
 // returns the exterior root, subject to change but should suffice for now?
 std::complex<double> JoukowskyTransform::w2z(std::complex<double> w) const {
--- joukowsky.h
+++ joukowsky.h
@@ -7,7 +7,12 @@ class JoukowskyTransform {
 public:
     JoukowskyTransform(double a, std::complex<double> b);
     std::complex<double> z2w(std::complex<double> z) const;
-    std::complex<double> w2z(std::complex<double> w) const; //returns two solns
+    std::complex<double> w2z(std::complex<double> w) const; // returns two solns
+    std::complex<double> dz2w(std::complex<double> z) const; // derivative dw/dz
+
+    double               getA() const { return a; }
+    std::complex<double> getB() const { return b; }
+    double               circleRadius() const { return std::abs(std::complex<double>(a, 0.0) - b); }
 
 private:
     double a;
